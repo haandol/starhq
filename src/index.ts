459f3eb7b588e2efba780b557b222aef0a8551ee
buzzni <buzzni@sally.local>
2018-06-28 02:08:53 +0900

    remove star depending interfaces

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,6 @@ export { IEvent } from './interface/event';
 export { Metadata } from './interface/metadata';
 export { Param } from './interface/param';
 
-export { Account } from './interface/account';
-export { Auth } from './interface/auth';
-export { Player } from './interface/player';
-export { Project } from './interface/project';
-export { Team } from './interface/team';
-export { Invitation } from './interface/invitation';
-export { Ticket } from './interface/ticket';
 export { Singleton } from './util/singleton';
 export { StarError } from './error';
 export { Decorator } from './decorator';
